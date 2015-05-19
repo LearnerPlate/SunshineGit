6a8dbc727c3a7235f80688ce3f4d8bc486e6a657
LearnerPlate <g60_turbo@wp.pl>
2015-05-19 12:21:46 +0200

    Sunshine app
    UDACITY android course

---
+++ Sunshine2/app/src/main/java/com/example/android/sunshine2/SettingsActivity.java
@@ -0,0 +1,20 @@
+package com.example.android.sunshine2;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+
+public class SettingsActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Display the fragment as the main content.
+        getFragmentManager().beginTransaction()
+                .replace(android.R.id.content, new SettingsFragment())
+                .commit();
+    }
+
+}
+
